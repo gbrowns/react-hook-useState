2d10ec624f80d93b7c4bf41d78b329b50d4bbbf1
Godwin254 <godwinbee15@gmail.com>
2020-06-05 14:34:05 -0400

    corrected errors

--- HookCounter2.js
+++ HookCounter2.js
@@ -2,14 +2,14 @@ import React, {useState} from 'react'
 
 
 function HookCounter2(){
-  initialCount = 0
+  const initialCount = 0
   const [count,setCount] = useState(initialCount);
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount(initialCount - 1)}>Decrease</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
       <button onClick={() => setCount(initialCount)}>Reset</button>
-      <button onClick={() => setCount(initialCount + 1)}>Increase</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
   )
 }
