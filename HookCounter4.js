0e883dcc3496a5053f41a3305142c9bd317a9e69
Godwin254 <godwinbee15@gmail.com>
2020-06-05 16:23:56 -0400

    added a method to generate random numbers and add into the array

--- HookCounter4.js
+++ HookCounter4.js
@@ -4,7 +4,12 @@ import React, {useState} from 'react'
 function HookCounter4(){
   const [items,setItems] = useState([]);
 
-
+  const addItem = () =>{
+    setItems([...items,{
+      id: items.length,
+      value: Math.floor(Math.random() * 10) + 1
+    }])
+  }
   return (
     <div>
       <button onClick={addItem}>Add number</button>
