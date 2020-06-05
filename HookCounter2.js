ff7937f85cea258e6ee6101478b5d8c8aef6f321
Godwin254 <godwinbee15@gmail.com>
2020-06-05 14:28:36 -0400

    added some jsx

--- HookCounter2.js
+++ HookCounter2.js
@@ -6,7 +6,10 @@ function HookCounter2(){
   const [count,setCount] = useState(initialCount);
   return (
     <div>
-
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(initialCount - 1)}>Decrease</button>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(initialCount + 1)}>Increase</button>
     </div>
   )
 }
