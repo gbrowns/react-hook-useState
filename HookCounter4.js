fb11e7960c5c771de3f4acf52ae3c04b7a2c79af
Godwin254 <godwinbee15@gmail.com>
2020-06-05 16:20:55 -0400

    defined the jsx

--- HookCounter4.js
+++ HookCounter4.js
@@ -3,9 +3,16 @@ import React, {useState} from 'react'
 
 function HookCounter4(){
   const [items,setItems] = useState([]);
+
+
   return (
     <div>
-    
+      <button onClick={addItem}>Add number</button>
+      <ul>
+        {
+          items.map(item => (<li key={item.id}>{item.value}</li>))
+        }
+      </ul>
     </div>
   )
 }
