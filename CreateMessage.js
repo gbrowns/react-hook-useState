5e3308d6971380f4166a7d86d3798f625ba598f6
Godwin254 <godwinbee15@gmail.com>
2020-06-05 16:46:10 -0400

    defined the jsx

--- CreateMessage.js
+++ CreateMessage.js
@@ -3,9 +3,34 @@ import React, {useState} from 'react'
 function CreateMessage(){
 
   const [message,setMessage] = useState({name: '',text: ''});
+
+  const handleChange = (e)=>{
+
+    setMessage({...message,[e.target.name]: e.target.value})
+  }
   return (
     <div className='msgpane'>
-      <input type ='text' value=''/>
+      <input 
+        type ='text' 
+        name='name' 
+        value={message.name} 
+        onChange={handleChange}
+      />
+
+      <textarea 
+        type ='text' 
+        name='text' 
+        value={message.text} 
+        onChange={handleChange}>
+      </textarea>
+
+      <button onClick={submitMsg}>submit</button>
+
+      <div className='displayMsg'>
+        {message}
+      </div>
+
+
     </div>
   )
 }
