c4e2a1dce713125387df11b90ee150919acd8a71
Godwin254 <godwinbee15@gmail.com>
2020-06-05 15:56:49 -0400

    defined the jsx

--- HookCounter3.js
+++ HookCounter3.js
@@ -2,10 +2,22 @@ import React, {useState} from 'react'
 
 function HookCounter3(){
   const [name,setName] = useState({firstName: '',lastName: ''})
-  
+
   return (
     <form>
-    
+      <input
+        type='text'
+        value={name.firstName}
+        onChange={e => setName({...name,firstName: e.target.value})}
+      />
+
+      <input
+        type='text'
+        value={name.lastName}
+        onChange={e => setName({...name,lastName: e.target.value})}
+      />
+
+      <h4>Your name is <b>{name.firstName} {name.lastName}</b></h4>
     </form>
   )
 }
