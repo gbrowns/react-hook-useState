03221e8598d10583a95af767dabc89eba6910453
Godwin254 <godwinbee15@gmail.com>
2020-06-08 11:01:48 -0400

    add jsx

--- NewCounter.js
+++ NewCounter.js
@@ -3,10 +3,21 @@ import React, {useState} from 'react'
 function NewCounter(){
 
   const [items,setItem] = useState([])
-  
+
   return (
     <div>
-
+        <input
+          type='text'
+          name='title'
+          value={}
+          onChange={handleChange}
+        />
+        <textarea 
+          name="message"
+          value={}
+          onChange={handleChange}
+        ></textarea>
+        <button type='button' onClick={addMessage}>Save message</button>  
     </div>
 
   )
